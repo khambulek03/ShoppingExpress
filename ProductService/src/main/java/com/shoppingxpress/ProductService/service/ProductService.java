ac6ca41228a327f89906063482c384940bd69f57
Ntandoyenkosi Mncube <khambulek03@gmail.com>
2026-08-17 15:38:21 +0200

    Update ProductService.java

---
+++ ProductService/src/main/java/com/shoppingxpress/ProductService/service/ProductService.java
@@ -0,0 +1,12 @@
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductResponseDTO getProduct(UUID id) {
+        return new ProductResponseDTO();
+    }
+}
