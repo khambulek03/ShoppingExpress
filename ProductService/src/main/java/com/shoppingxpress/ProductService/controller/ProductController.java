b4f946ed06ed0dd3f6b28d5e89f95207b64485f1
Ntandoyenkosi Mncube <khambulek03@gmail.com>
2026-08-17 15:11:02 +0200

    Update ProductController.java

---
+++ ProductService/src/main/java/com/shoppingxpress/ProductService/controller/ProductController.java
@@ -0,0 +1,27 @@
+package com.shoppingxpress.ShoppingService.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+
+import lombok.RequiredArgsConstructor;
+
+
+
+@RestController
+@RequestMapping("/api/product")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getProduct(@PathVariable UUID id) {
+        ProductResponseDTO product = productService.getProduct(id);
+        return ResponseEntity.ok(product);
+    }
+}
